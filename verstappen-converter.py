31737307d6e4f3ab740180174526c968becbcf63
Filip Řeháček <filiprehacek4@gmail.com>
2026-05-25 23:51:56 +0200

    Stress is very bad for you, and you're going to die sooner if you have a lot of stress. So I'm going to be 250 years old.

--- verstappen-converter.py
+++ verstappen-converter.py
@@ -3,27 +3,41 @@
 from argparse import ArgumentParser
 from random import choice
 
-
 VERSTAPPEN_NUMBER = 69
-PLACEHOLDER = "%formatted_number%"
 QUOTES = [
-    f"Mate, I don't care about the calculations, the delta is exactly {PLACEHOLDER}, simply lovely... now I'm going back to my sim rig.",
-    f"Mate, the car feels completely undriveable, jumping around like a kangaroo, but somehow we are {PLACEHOLDER} tenths ahead, haha, yes boys!",
-    f"GP, my tires are completely gone, but the gap to P2 is still {PLACEHOLDER} seconds, simply lovely.",
-    f"I've been doing 24-hour endurance races on iRacing all night, so calculating {PLACEHOLDER} is the hardest thing I've done all weekend.",
-    f"Christian, you can tell the stewards to give me a ten-second penalty, I'll still finish {PLACEHOLDER} seconds in front of everyone else.",
-    f"So basically we just survived turn one, and after that the pace was a solid {PLACEHOLDER} out of 10, just an unbelievable race."
+    "Mate, I don't care about the calculations, the delta is exactly {}, simply lovely... now I'm going back to my sim rig.",
+    "Mate, the car feels completely undriveable, jumping around like a kangaroo, but somehow we are {} tenths ahead, haha, yes boys!",
+    "GP, my tires are completely gone, but the gap to P2 is still {} seconds, simply lovely.",
+    "I've been doing 24-hour endurance races on iRacing all night, so calculating {} is the hardest thing I've done all weekend.",
+    "Christian, you can tell the stewards to give me a ten-second penalty, I'll still finish {} seconds in front of everyone else.",
+    "So basically we just survived turn one, and after that the pace was a solid {} out of 10, just an unbelievable race."
 ]
 
 
-
-def convert_number(number) -> tuple[int, int]:
+def convert_number(number: int) -> tuple[int, int]:
     multiplication = number // VERSTAPPEN_NUMBER
     addition = number % VERSTAPPEN_NUMBER
     return multiplication, addition
 
 
-if __name__ == "__main__":
+def format_string(multiplication: int, addition: int) -> str:
+    str_output = ""
+    match multiplication:
+        case 1:
+            str_output = f"{VERSTAPPEN_NUMBER}"
+        case -1:
+            str_output = f"-{VERSTAPPEN_NUMBER}"
+        case _:
+            str_output = f"{multiplication} * {VERSTAPPEN_NUMBER}"
+
+    if addition > 0:
+        str_output += f" + {addition}"
+    elif addition < 0:
+        str_output += f" - {abs(addition)}"
+    return str_output
+
+
+def main() -> None:
     parser = ArgumentParser(
         description="Convert a number to Max Verstappen's number format."
     )
@@ -32,18 +46,19 @@ if __name__ == "__main__":
         "-r",
         "--result-only",
         action="store_true",
-        default=False,
         help="Only display the converted number.",
     )
     args = parser.parse_args()
-    args.result_only = args.result_only or False
 
     multiplication, addition = convert_number(args.number)
-    str_output = f"{multiplication} * {VERSTAPPEN_NUMBER} + {addition}"
+    str_output = format_string(multiplication, addition)
 
     if args.result_only:
         print(str_output)
     else:
-        quote = choice(QUOTES)
-        quote = quote.replace(PLACEHOLDER, str_output)
+        quote = choice(QUOTES).format(str_output)
         print(quote)
+
+
+if __name__ == "__main__":
+    main()
